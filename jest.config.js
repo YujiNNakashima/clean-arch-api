62bad4ad189fbe30356eb37e16e4d601b5c6dc2f
Yuji <yujiricardo@gmail.com>
2020-07-13 16:11:49 -0300

    feat: ensure Signup Controllers returns 400 if name doesnt exist

--- jest.config.js
+++ jest.config.js
@@ -2,13 +2,17 @@ module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
     '!<rootDir>/src/**/*-protocols.ts',
-    '!**/protocols/**'
+    '!**/protocols/**',
+    '!**/test/**'
   ],
   coverageDirectory: 'coverage',
-  coverageProvider: 'v8',
   testEnvironment: 'node',
   transform: {
-    '.+\\.tss': 'ts-jest'
+    '.+\\.ts$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1'
   }
 }
