f125d33f4832d046a383cf20d1719a01ccb128ce
Yuji <yujiricardo@gmail.com>
2020-07-23 21:27:01 -0300

    test: success case add account

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -17,7 +17,7 @@ describe('DBAddAccount Usecase', () => {
       async add (accountData: AddAccountModel): Promise<AccountModel> {
         const fakeAccount = {
           name: 'valid_name',
-          email: 'email@email.com',
+          email: 'valid_email',
           password: 'hashed_password',
           id: 'valid_id'
         }
@@ -97,5 +97,21 @@ describe('DBAddAccount Usecase', () => {
     }
     const promise = sut.add(accountData)
     await expect(promise).rejects.toThrow()
+  })
+
+  test('should return an account on success', async () => {
+    const { sut } = makeSut()
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password'
+    }
+    const account = await sut.add(accountData)
+    expect(account).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'hashed_password'
+    })
   })
 })
