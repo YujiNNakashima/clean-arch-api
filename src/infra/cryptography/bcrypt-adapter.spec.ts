7f4fd9b26bc781de8ec55e696b16f5e8a7cc3692
Yuji <yujiricardo@gmail.com>
2020-07-24 09:34:07 -0300

    test: bcrypt hash success

--- src/infra/cryptography/bcrypt-adapter.spec.ts
+++ src/infra/cryptography/bcrypt-adapter.spec.ts
@@ -1,12 +1,25 @@
 import bcrypt from 'bcrypt'
 import { BcryptAdapter } from './bcrypt-adapter'
 
+jest.mock('bcrypt', () => ({
+  async hash (): Promise<string> {
+    return new Promise(resolve => resolve('valid_hash'))
+  }
+}))
+
 describe('Bcrypt adapter test', () => {
-  test('test hash success', async () => {
+  test('test call bcrypt success', async () => {
     const salt = 12
     const sut = new BcryptAdapter(salt)
     const hashSpy = jest.spyOn(bcrypt, 'hash')
     await sut.encrypt('any_value')
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
+  })
+
+  test('test hash success', async () => {
+    const salt = 12
+    const sut = new BcryptAdapter(salt)
+    const hash = await sut.encrypt('any_value')
+    expect(hash).toBe('valid_hash')
   })
 })
