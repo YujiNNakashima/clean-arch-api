3a76c5bb453b96afea3c0287096b0420b4543533
Yuji <yujiricardo@gmail.com>
2020-07-27 12:55:55 -0300

    feat: decorator log

---
+++ src/main/decorators/log.ts
@@ -0,0 +1,18 @@
+import { HttpRequest, HttpResponse } from '../../presentation/protocols/http'
+import { Controller } from '@/presentation/protocols'
+
+export class LogControllerDecorator implements Controller {
+  private readonly controller: Controller
+
+  constructor (controller: Controller) {
+    this.controller = controller
+  }
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    const httpResponse = await this.controller.handle(httpRequest)
+    if (httpResponse.statusCode === 500) {
+      // log
+    }
+    return httpResponse
+  }
+}
