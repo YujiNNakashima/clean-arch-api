a6ab303b811a8879e0613bf914cb49c5d952e314
Yuji <yujiricardo@gmail.com>
2020-08-02 10:37:33 -0300

    test: ensure required field validation return expected results

---
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -0,0 +1,16 @@
+import { RequiredFieldValidation } from './required-field-validation'
+import { MissingParamError } from '../../errors/missing-param-error'
+
+describe('RequiredFieldValidation', () => {
+  test('should return a MissingParamError if validation fails', () => {
+    const sut = new RequiredFieldValidation('any_field')
+    const error = sut.validate({ name: 'any_name' })
+    expect(error).toEqual(new MissingParamError('any_field'))
+  })
+
+  test('should not return if validation succeeds', () => {
+    const sut = new RequiredFieldValidation('field')
+    const error = sut.validate({ field: 'any_name' })
+    expect(error).toBeFalsy()
+  })
+})
