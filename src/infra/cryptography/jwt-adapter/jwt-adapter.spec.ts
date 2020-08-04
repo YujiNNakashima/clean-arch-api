38dff5fbbe443352b6da5aef56e690083a3c2592
Yuji <yujiricardo@gmail.com>
2020-08-04 14:24:06 -0300

    test: jwt test

--- src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
@@ -2,6 +2,12 @@ import { Encrypter } from '../../../data/protocols/cryptography/encrypter'
 import { JwtAdapter } from './jwt-adapter'
 import jwt from 'jsonwebtoken'
 
+jest.mock('jsonwebtoken', () => ({
+  async sign (): Promise<string> {
+    return new Promise(resolve => resolve('any_token'))
+  }
+}))
+
 describe('JwtAdapter', () => {
   interface SutTypes {
     sut: Encrypter
@@ -19,5 +25,11 @@ describe('JwtAdapter', () => {
     const signSpy = jest.spyOn(jwt, 'sign')
     await sut.encrypt('any_id')
     expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secret')
+  })
+
+  test('should return a token on sign success', async () => {
+    const { sut } = makeSut()
+    const token = await sut.encrypt('any_id')
+    expect(token).toBe('any_token')
   })
 })
