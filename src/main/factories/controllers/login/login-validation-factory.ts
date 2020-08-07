3e414e746bd480f8ef00cc3cd5a257621ae09411
Yuji <yujiricardo@gmail.com>
2020-08-07 20:52:38 -0300

    refactor: folders and folders

---
+++ src/main/factories/controllers/login/login-validation-factory.ts
@@ -0,0 +1,12 @@
+import { RequiredFieldValidation } from '../../../../presentation/helpers/validators/required-field-validation'
+import { EmailValidation } from '../../../../presentation/helpers/validators/email-validation'
+import { EmailValidatorAdapter } from '../../../../utils/email-validator-adapter'
+import { ValidationComposite } from '../../../../presentation/helpers/validators/validation-composite'
+
+export const makeLoginValidation = (): ValidationComposite => {
+  return new ValidationComposite([
+    new RequiredFieldValidation('email'),
+    new RequiredFieldValidation('password'),
+    new EmailValidation('email', new EmailValidatorAdapter())
+  ])
+}
